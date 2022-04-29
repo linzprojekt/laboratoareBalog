a79a63fb25d582490a9f47955c5e9e3fe60cb9b1
linzprojekt <linz.projekt@gmail.com>
2022-04-29 13:51:57 +0300

    last change

---
+++ laboratoareBalog/laboratoareBalog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laboratoareBalog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
